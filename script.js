4779a5eaad72be5dca99123f0f32e08c35165c3e
Ana Paula Souza Dias <anapaula_sdias@hotmail.com>
2020-12-21 21:24:54 -0300

    more information

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-function verificar(){
-  var prim = document.getElementsByName('first')
-  var primeiro= prim.value;
-  var ult = document.getElementsByName('last')
-  var ultimo = ult.value;
-  var pulando = document.getElementsByName('pulando')
-  var res = document.getElementById('res')
-  var soma = primeiro+ultimo;
-  res.innerHTML = `A soma é ${soma}`
+function contar(){
+  let inicio = document.getElementsById('txti');
+  let fim = document.getElementsById('txtf');
+  let passo = document.getElementsById('txtp');
+  let res = document.getElementById('res');
+  res.innerHTML = `Contando`;
+  let comeco= Number(inicio.value);
+  let final = Number(fim.value);
+  let passando = Number(passo.value);
+  var soma = comeco + final;
+  res.innerHTML = `A soma é ${soma}`;
 }
+
+ 
