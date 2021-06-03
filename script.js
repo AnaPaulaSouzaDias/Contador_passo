ed899bd89d3c6a0921a04dd8f74c34be12e189b5
Ana Paula Souza Dias <anapaula_sdias@hotmail.com>
2021-06-03 20:11:47 -0300

    finish

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-function contar(){
-  let inicio = document.getElementsById('txti');
-  let fim = document.getElementsById('txtf');
-  let passo = document.getElementsById('txtp');
-  let res = document.getElementById('res');
-  res.innerHTML = `Contando`;
-  let comeco= Number(inicio.value);
-  let final = Number(fim.value);
-  let passando = Number(passo.value);
-  var soma = comeco + final;
-  res.innerHTML = `A soma é ${soma}`;
-}
+function contar() {
+  let comeco = document.getElementById('inicio')
+  let final = document.getElementById('fim')
+  let passada = document.getElementById('passo')
+  let res = document.getElementById('res')
 
- 
+  if (comeco.value.length == 0 || final.value.length == 0 || passada.value.length == 0) {
+    res.innerHTML = 'Impossivel contar'
+  } else {
+    res.innerHTML = 'Contando: '
+    let i = Number(comeco.value)
+    let f = Number(final.value)
+    let p = Number(passada.value)
+    if (p <= 0) {
+      window.alert('Passo invalido, considerando Passo 1')
+      p = 1
+    }
+    if (i < f) {
+      // Contagem crescente
+      for (let cont = i; cont <= f; cont += p) {
+        res.innerHTML += ` ${cont} \u{1F680}`
+      }
+    } else {
+      //Contagem descrecente
+      for (let cont = i; cont >= f; cont -= p) {
+        res.innerHTML += ` ${cont} \u{1F680}`
+      }
+    }
+    res.innerHTML += `\u{1F3C1}`
+  }
+}
